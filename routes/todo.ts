4e7358a70fd191074fb4f7aad1b2927d9e446e52
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-08-18 10:32:07 +0530

    finishing REST routes

--- routes/todo.ts
+++ routes/todo.ts
@@ -2,7 +2,7 @@ import {Router} from 'express';
 
 import { Todo } from '../models/todo';
 
-const todos: Todo[]  = [];
+let todos: Todo[]  = [];
 
 const router = Router();
 
@@ -16,6 +16,29 @@ router.post('/todo' , (req , res , next) => {
         text : req.body.text
     };
     todos.push(newTodo);
+    res.status(201).json({message : 'Added TODO' , todo : newTodo , todos : todos});
 });
+
+router.put('/todo/:todoId' , (req , res , next) => {
+    const tid = req.params.todoId;
+
+    // find index of some element
+    // want to return true if todoItem.id === tid
+    const todoIndex = todos.findIndex(todoItem => todoItem.id === tid );
+    if(todoIndex >= 0) {
+        //valid index
+        todos[todoIndex] = { id : todos[todoIndex].id , text : req.body.text} ;
+        return res.status(200).json({message : 'updated todo ' , todos : todos});
+    }
+    //invalid index
+    res.status(404).json({message : 'Could not find todo for this id '});
+});
+
+router.delete('/todo/:todoId' , (req , res , next) => {
+    //get rid of one todo
+    todos = todos.filter(todoItem => todoItem.id !== req.params.todoId);
+    res.status(200).json({message : 'Deleted TODO' , todos : todos});
+});
+
 
 export default router;
